23abe0cc8c01e22efb6b78cc1eed37108bdcd11e
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2025-01-05 21:46:11 +0600

    added muteable concept

--- test.js
+++ test.js
@@ -6,13 +6,31 @@ console.log(sum(5, 6)); // 11
 console.log(sum(5, 6)); // 11
 console.log(sum(5, 6)); // 11
 
-// Impure Function: A function is considered impure if it relies on external state 
+// Impure Function: A function is considered impure if it relies on external state
 // or modifies external variables, leading to potential side effects.
 let total = 2;
 
 const addToTotal = (amount) => {
-    return total = total + amount; // Modifies the external variable 'total', making it impure.
-}
+  return (total = total + amount); // Modifies the external variable 'total', making it impure.
+};
 
 console.log(addToTotal(5)); // 7
 console.log(addToTotal(5)); // 12
+
+const updateDate = () => {
+  return new Date(); // Returns a new Date object, which is an impure function.
+};
+
+// mutation
+
+const employee = {
+  name: "John Doe",
+  age: 30,
+  salary: 50000,
+  address : {country : "United States", city : "new York"}
+};
+
+const employee2 = { ...employee, name: "Md Saiful Islam" , address : {...employee.address , country : "Bangladesh", city : "Dhaka" } };
+
+console.log(employee);
+console.log(employee2);
