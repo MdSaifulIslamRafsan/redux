d7b1970012faa5abd6bbf58147baeff2f64c2992
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2025-01-06 19:28:40 +0600

    added handle dark mood light mood useing shadcn

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggle";
 
 const Navbar = () => {
-    return (
-        <nav className="max-w-7xl mx-auto h-16 flex item-center gap-3 px-5">
-            <div className="flex items-center">
-                <span className="font-bold ml-2">Task</span> Master
-            </div>
-            <Link to="/">Tasks</Link>
-            <Link to="/users">Users</Link>
-        </nav>
-    );
+  return (
+    <nav className="max-w-7xl mx-auto h-16 flex item-center gap-3 px-5 justify-between">
+      <div className="flex space-x-5 items-center">
+       <h2 className="text-3xl"> <span className="font-bold ml-2">Task</span> Master</h2>
+        <Link to="/">Tasks</Link>
+        <Link to="/users">Users</Link>
+      </div>
+      <div className="flex items-center">
+        <ModeToggle></ModeToggle>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router";
-import  router  from "./routes/route.tsx";
-
+import router from "./routes/route.tsx";
+import { ThemeProvider } from "./provider/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-    <RouterProvider router={router} />
-    </Provider>
+    <ThemeProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
