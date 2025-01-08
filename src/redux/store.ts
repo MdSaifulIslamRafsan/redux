20ab82f98e2517eea765e5ce998ac9b658998c68
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2025-01-09 02:18:31 +0600

    added taskApi data get using redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,13 +3,16 @@ import counterReducer from "./features/counter/counterSlice"
 import taskReducer from "./features/task/taskSlice"
 import userReducer from "./features/user/userSlice"
 import logger from "./middlewares/logger";
+import { taskApi } from "./api/taskApi";
 export const store = configureStore({
     reducer : {
         counter : counterReducer,
         todo : taskReducer,
         user : userReducer,
+        [taskApi.reducerPath] : taskApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(taskApi.middleware).concat(logger)
+    
 })
 
 export type RootState =  ReturnType<typeof store.getState>
